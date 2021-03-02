92aa311ffc1e78cd250403de9761fdcd11299bae
chenlin <chenlin042@gmail.com>
2021-03-02 22:02:36 +0800

    增加自定义配置项示例

--- src/main/java/com/chenlin/wiki/controller/TestController.java
+++ src/main/java/com/chenlin/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.chenlin.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
