5107add02471839dd0adb566879453d6b4d97dde
chenlin <chenlin042@gmail.com>
2021-03-02 20:37:22 +0800

    增加hello接口测试test.http

--- src/main/java/com/chenlin/wiki/controller/TestController.java
+++ src/main/java/com/chenlin/wiki/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.chenlin.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post " + name;
     }
 }
