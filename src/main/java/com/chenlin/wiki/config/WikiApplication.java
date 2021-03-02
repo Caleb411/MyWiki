d74bbd979f0efd65e3bd73e86700a4ac6728b50c
chenlin <chenlin042@gmail.com>
2021-03-02 19:33:57 +0800

    开发Hello World接口

--- src/main/java/com/chenlin/wiki/config/WikiApplication.java
+++ src/main/java/com/chenlin/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.chenlin.wiki;
+package com.chenlin.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.chenlin")
 @SpringBootApplication
 public class WikiApplication {
 
---
+++ src/main/java/com/chenlin/wiki/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.chenlin.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
