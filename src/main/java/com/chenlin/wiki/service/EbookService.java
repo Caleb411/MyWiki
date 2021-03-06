190f9114148262eedda589795069761e0dcad325
chenlin <chenlin042@gmail.com>
2021-03-06 21:02:29 +0800

    完成电子书列表查询接口

---
+++ src/main/java/com/chenlin/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.chenlin.wiki.service;
+
+import com.chenlin.wiki.domain.Ebook;
+import com.chenlin.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
