60dd2f04e3722191f3fe75b9cb8040eddb021a80
chenlin <chenlin042@gmail.com>
2021-03-06 21:20:15 +0800

    增加通用的返回类CommonResp

--- src/main/java/com/chenlin/wiki/controller/EbookController.java
+++ src/main/java/com/chenlin/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.chenlin.wiki.controller;
 
 import com.chenlin.wiki.domain.Ebook;
+import com.chenlin.wiki.resp.CommonResp;
 import com.chenlin.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
