aa1eb04d6a8a72388e09223f01880f0b155d09e5
chenlin <chenlin042@gmail.com>
2021-03-27 12:08:22 +0800

    配置axios拦截器打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -79,17 +79,14 @@
   export default defineComponent({
     name: 'Home',
     setup() {
-      console.log("setup");
       const ebooks = ref();
       const ebooks1 = reactive({books: []});
 
       onMounted(() => {
-        console.log("onMounted222");
         axios.get("/ebook/list").then((response) => {
           const data = response.data;
           ebooks.value = data.content;
           ebooks1.books = data.content;
-          console.log(response);
         });
       });
 
