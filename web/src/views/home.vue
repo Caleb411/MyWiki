db2d545af4f91dd37a012bce99ca76f5330efa68
Caleb411 <chenlin042@gmail.com>
2021-04-10 21:17:01 +0800

    首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -2,40 +2,28 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-              mode="inline"
-              :style="{ height: '100%', borderRight: 0 }"
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span><user-outlined />subnav 11111</span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span><laptop-outlined />subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span><notification-outlined />subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -60,67 +48,98 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
-  import axios from 'axios';
+import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
-  // const listData: any = [];
-  // for (let i = 0; i < 23; i++) {
-  //   listData.push({
-  //     href: 'https://www.antdv.com/',
-  //     title: `ant design vue part ${i}`,
-  //     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-  //     description:
-  //             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-  //     content:
-  //             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  //   });
-  // }
+// const listData: any = [];
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
-  export default defineComponent({
-    name: 'Home',
-    setup() {
-      const ebooks = ref();
-      // const ebooks1 = reactive({books: []});
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const ebooks = ref();
+    // const ebooks1 = reactive({books: []});
 
-      onMounted(() => {
-        axios.get("/ebook/list", {
-          params: {
-            page: 1,
-            size: 1000
-          }
-        }).then((response) => {
-          const data = response.data;
-          ebooks.value = data.content.list;
-          // ebooks1.books = data.content;
-        });
+    const level1 =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
       });
+    };
 
-      return {
-        ebooks,
-        // ebooks2: toRef(ebooks1, "books"),
-        // listData,
-        pagination: {
-          onChange: (page: any) => {
-            console.log(page);
-          },
-          pageSize: 3,
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then((response) => {
+        const data = response.data;
+        ebooks.value = data.content.list;
+        // ebooks1.books = data.content;
+      });
+    });
+
+    return {
+      ebooks,
+      // ebooks2: toRef(ebooks1, "books"),
+      // listData,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
         },
-        actions: [
-          { type: 'StarOutlined', text: '156' },
-          { type: 'LikeOutlined', text: '156' },
-          { type: 'MessageOutlined', text: '2' },
-        ],
-      }
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
+
+      handleClick,
+      level1,
     }
-  });
+  }
+});
 </script>
 
 <style scoped>
-  .ant-avatar {
-    width: 50px;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 8%;
-    margin: 5px 0;
-  }
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
 </style>
