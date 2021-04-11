8546a202ff1ab62b4e1656a8f185a09dc590944f
Caleb411 <chenlin042@gmail.com>
2021-04-11 13:27:01 +0800

    点击菜单实现首页欢迎页面和电子书页面的切换

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,10 +7,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
-            <MailOutlined />
-            <span>欢迎</span>
-          </router-link>
+          <MailOutlined />
+          <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
@@ -25,7 +23,10 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎使用知识库系统</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -93,8 +94,11 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("menu click")
+    const isShowWelcome = ref(true);
+
+    const handleClick = (value: any) => {
+      // console.log("menu click", value)
+      isShowWelcome.value = value.key === 'welcome';
     };
 
     onMounted(() => {
@@ -129,6 +133,8 @@ export default defineComponent({
 
       handleClick,
       level1,
+
+      isShowWelcome
     }
   }
 });
