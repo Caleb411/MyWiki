5d9730b8bd548ac75a9b4e056b2de1fec022db34
Caleb411 <chenlin042@gmail.com>
2021-04-13 10:03:45 +0800

    集成富文本框插件，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -80,6 +80,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -91,6 +94,7 @@
   import {Tool} from "@/util/tool";
   import {useRoute} from "vue-router";
   import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+  import E from "wangeditor";
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -155,6 +159,8 @@
       const doc = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
+      const editor = new E("#content")
+
       const handleModalOk = () => {
         modalLoading.value = true;
         axios.post("/doc/save", doc.value).then((response) => {
@@ -251,6 +257,10 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
+
+        setTimeout(function () {
+          editor.create();
+        },100)
       };
 
       /**
@@ -266,6 +276,10 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
+
+        setTimeout(function () {
+          editor.create();
+        },100)
       };
 
       /**
