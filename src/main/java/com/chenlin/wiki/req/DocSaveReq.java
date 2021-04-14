702fdd7fc63acebde5f137866c9dd6216f51eafb
Caleb411 <chenlin042@gmail.com>
2021-04-14 10:54:32 +0800

    增加保存文档内容功能

--- src/main/java/com/chenlin/wiki/req/DocSaveReq.java
+++ src/main/java/com/chenlin/wiki/req/DocSaveReq.java
@@ -22,6 +22,17 @@ public class DocSaveReq {
 
     private Integer voteCount;
 
+    @NotNull(message = "【内容】不能为空")
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public Long getId() {
         return id;
     }
@@ -80,18 +91,15 @@ public class DocSaveReq {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", ebookId=").append(ebookId);
-        sb.append(", parent=").append(parent);
-        sb.append(", name=").append(name);
-        sb.append(", sort=").append(sort);
-        sb.append(", viewCount=").append(viewCount);
-        sb.append(", voteCount=").append(voteCount);
-        sb.append("]");
-        return sb.toString();
+        return "DocSaveReq{" +
+                "id=" + id +
+                ", ebookId=" + ebookId +
+                ", parent=" + parent +
+                ", name='" + name + '\'' +
+                ", sort=" + sort +
+                ", viewCount=" + viewCount +
+                ", voteCount=" + voteCount +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
--- src/main/java/com/chenlin/wiki/service/DocService.java
+++ src/main/java/com/chenlin/wiki/service/DocService.java
@@ -1,7 +1,9 @@
 package com.chenlin.wiki.service;
 
+import com.chenlin.wiki.domain.Content;
 import com.chenlin.wiki.domain.Doc;
 import com.chenlin.wiki.domain.DocExample;
+import com.chenlin.wiki.mapper.ContentMapper;
 import com.chenlin.wiki.mapper.DocMapper;
 import com.chenlin.wiki.req.DocQueryReq;
 import com.chenlin.wiki.req.DocSaveReq;
@@ -26,6 +28,9 @@ public class DocService {
     
     @Resource
     private DocMapper docMapper;
+
+    @Resource
+    private ContentMapper contentMapper;
 
     @Resource
     private SnowFlake snowFlake;
@@ -69,13 +74,21 @@ public class DocService {
      */
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
+        Content content = CopyUtil.copy(req, Content.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             // 新增
             doc.setId(snowFlake.nextId());
             docMapper.insert(doc);
+
+            content.setId(doc.getId());
+            contentMapper.insert(content);
         } else {
             // 更新
             docMapper.updateByPrimaryKey(doc);
+            int count = contentMapper.updateByPrimaryKeyWithBLOBs(content); // 包含大字段
+            if (count == 0) {
+                contentMapper.insert(content);
+            }
         }
     }
 
