4f103811772c18a2dba38fe35b12c8ef8ae4dcee
Caleb411 <chenlin042@gmail.com>
2021-04-18 21:29:55 +0800

    增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/chenlin/wiki/controller/ControllerExceptionHandler.java
+++ src/main/java/com/chenlin/wiki/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.chenlin.wiki.controller;
 
+import com.chenlin.wiki.exception.BusinessException;
 import com.chenlin.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +24,41 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler(value = BindException.class)
     @ResponseBody
-    public CommonResp validExceptionHandler(BindException e) {
-        CommonResp commonResp = new CommonResp();
+    public CommonResp<?> validExceptionHandler(BindException e) {
+        CommonResp<?> commonResp = new CommonResp<>();
         LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+
+    /**
+     * 业务异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp<?> validExceptionHandler(BusinessException e) {
+        CommonResp<?> commonResp = new CommonResp<>();
+        LOG.warn("业务异常：{}", e.getCode().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getCode().getDesc());
+        return commonResp;
+    }
+
+    /**
+     * 系统异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp<?> validExceptionHandler(Exception e) {
+        CommonResp<?> commonResp = new CommonResp<>();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员");
         return commonResp;
     }
 }
--- src/main/java/com/chenlin/wiki/service/UserService.java
+++ src/main/java/com/chenlin/wiki/service/UserService.java
@@ -1,6 +1,8 @@
 package com.chenlin.wiki.service;
 
 import com.chenlin.wiki.domain.*;
+import com.chenlin.wiki.exception.BusinessException;
+import com.chenlin.wiki.exception.BusinessExceptionCode;
 import com.chenlin.wiki.mapper.UserMapper;
 import com.chenlin.wiki.req.UserQueryReq;
 import com.chenlin.wiki.req.UserSaveReq;
@@ -13,6 +15,7 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -68,9 +71,15 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
@@ -79,5 +88,17 @@ public class UserService {
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
