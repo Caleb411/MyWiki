37e3e18e8af744c86d6a00728109729fa5e2d8a3
Caleb411 <chenlin042@gmail.com>
2021-04-23 17:34:35 +0800

    增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

--- src/main/java/com/chenlin/wiki/controller/UserController.java
+++ src/main/java/com/chenlin/wiki/controller/UserController.java
@@ -80,4 +80,12 @@ public class UserController {
         userService.delete(id);
         return resp;
     }
+
+    @GetMapping("/logout/{token}")
+    public CommonResp<?> logout(@PathVariable String token) {
+        CommonResp<?> resp = new CommonResp<>();
+        redisTemplate.delete(token);
+        LOG.info("从redis中删除token:{}", token);
+        return resp;
+    }
 }
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,6 +21,16 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
       <a class="login-menu" v-show="user.id">
         <span>你好，{{ user.name }}</span>
       </a>
@@ -91,13 +101,28 @@ export default defineComponent({
       });
     };
 
+    // 退出登录
+    const logout = () => {
+      console.log("退出登录开始");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {}); // 将全局变量置空
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     return {
       loginModalVisible,
       loginModalLoading,
       showLoginModal,
       loginUser,
       login,
-      user
+      user,
+      logout
     }
   }
 });
@@ -107,5 +132,6 @@ export default defineComponent({
 .login-menu {
   float: right;
   color: white;
+  padding-left: 10px;
 }
 </style>
