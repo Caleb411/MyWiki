28ebddb254596fae80b2168ebb110b1d2291318f
Caleb411 <chenlin042@gmail.com>
2021-04-23 21:36:59 +0800

    前后端增加登录拦截 修复BUG：前后端存储token统一用String类型

--- src/main/java/com/chenlin/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/chenlin/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,29 @@
-//package com.chenlin.wiki.config;
-//
-//import com.chenlin.wiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+package com.chenlin.wiki.config;
+
+import com.chenlin.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
