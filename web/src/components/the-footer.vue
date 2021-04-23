a22485a04a4f59b2d8bc3828b53338bfba385a5c
Caleb411 <chenlin042@gmail.com>
2021-04-23 11:54:06 +0800

    登录成功后使用vuex保存全局用户信息，方便其他组件获取，但刷新会没有

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,20 @@
 <template>
     <a-layout-footer style="text-align: center">
-        陈霖の电子书
+        {{user.name}}の电子书
     </a-layout-footer>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
     export default defineComponent({
-        name: 'the-footer'
+      name: 'the-footer',
+      setup() {
+        const user = computed(() => store.state.user);
+        return {
+          user
+        }
+      }
     });
 </script>
