e00c8ae7c5f1ae1685285a8add8c6a16d8487d28
Caleb411 <chenlin042@gmail.com>
2021-04-24 14:08:38 +0800

    点击文档节点时，加载并显示文档内容

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -13,6 +13,7 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -31,6 +32,7 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -63,13 +65,94 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        // 加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
+    };
+
     onMounted(() => {
       handleQuery();
     });
 
     return {
       level1,
+      html,
+      onSelect
     }
   }
 });
 </script>
+
+<style>
+  /* wangeditor默认样式, 参照: http://www.wangeditor.com/doc/pages/02-%E5%86%85%E5%AE%B9%E5%A4%84%E7%90%86/03-%E8%8E%B7%E5%8F%96html.html */
+  /* table 样式 */
+  .wangeditor table {
+    border-top: 1px solid #ccc;
+    border-left: 1px solid #ccc;
+  }
+  .wangeditor table td,
+  .wangeditor table th {
+    border-bottom: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+    padding: 3px 5px;
+  }
+  .wangeditor table th {
+    border-bottom: 2px solid #ccc;
+    text-align: center;
+  }
+
+  /* blockquote 样式 */
+  .wangeditor blockquote {
+    display: block;
+    border-left: 8px solid #d0e5f2;
+    padding: 5px 10px;
+    margin: 10px 0;
+    line-height: 1.4;
+    font-size: 100%;
+    background-color: #f1f1f1;
+  }
+
+  /* code 样式 */
+  .wangeditor code {
+    display: inline-block;
+    *display: inline;
+    *zoom: 1;
+    background-color: #f1f1f1;
+    border-radius: 3px;
+    padding: 3px 5px;
+    margin: 0 3px;
+  }
+  .wangeditor pre code {
+    display: block;
+  }
+
+  /* ul ol 样式 */
+  .wangeditor ul, ol {
+    margin: 10px 0 10px 20px;
+  }
+
+  /* 和antdv p冲突，覆盖掉 */
+  .wangeditor blockquote p {
+    font-family:"YouYuan";
+    margin: 20px 10px !important;
+    font-size: 16px !important;
+    font-weight:600;
+  }
+</style>
