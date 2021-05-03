1d354a7e2a70726f28ecb6b825a1baba3f886d39
chenlin <chenlin042@gmail.com>
2021-05-03 13:59:19 +0800

    增加日志流水号，方便生产运维

--- src/main/java/com/chenlin/wiki/aspect/LogAspect.java
+++ src/main/java/com/chenlin/wiki/aspect/LogAspect.java
@@ -3,17 +3,20 @@ package com.chenlin.wiki.aspect;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import com.chenlin.wiki.util.RequestContext;
+import com.chenlin.wiki.util.SnowFlake;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +31,14 @@ public class LogAspect {
     @Pointcut("execution(public * com.chenlin.*.controller..*Controller.*(..))")
     public void controllerPointcut() {}
 
+    @Resource
+    private SnowFlake snowFlake;
+
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
+
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
