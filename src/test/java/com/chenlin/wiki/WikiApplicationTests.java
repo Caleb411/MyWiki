bf8936d86982c5f3846580f64f61b58125b512c9
chenlin <chenlin042@gmail.com>
2021-05-04 20:33:28 +0800

    Spring多环境配置，生产发布修改

--- src/test/java/com/chenlin/wiki/WikiApplicationTests.java
+++ src/test/java/com/chenlin/wiki/WikiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.chenlin.wiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WikiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.chenlin.wiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WikiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
